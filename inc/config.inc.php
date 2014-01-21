4814f902fe99a1f60b36f7f664b5cca89bd5f68c
pwarnimo <pwarnimo@gmail.com>
2014-01-21 21:50:59 +0100

    push 1

---
+++ inc/config.inc.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * Vectra Drawing Program
+ *    Collaborative Server v0.1
+ *
+ * Filename : config.inc.php
+ * Author   : Pol Warnimont
+ *
+ * Description : This file is used to configure the database access.
+ */
+
+define("hostname", "localhost");
+define("username", "sqlusr");
+define("password", "q1w2e3!");
+define("database", "Vectra");
+define("dbtype"  , "mysql");
+
