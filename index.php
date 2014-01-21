cf1e6ba59f069c69fbaf72609b418c4460a3332c
pwarnimo <pwarnimo@gmail.com>
2014-01-21 21:50:06 +0100

    first commit wqe

--- index.php
+++ index.php
@@ -3,4 +3,4 @@ include "inc/vectra-srv.class.php";
 
 $vcs = new VectraServer();
 
-echo "<pre><img style=\"float:left; margin-right:5px;\" src=\"img/vectra.png\">VECTRA SERVER 0.1<br>" . date("Y\-m\-d H\:i\:s") . "<br>Copyright &copy;2014 Warnimont Pol</pre>";
+echo "<pre><img style=\"float:left; margin-right:5px;\" src=\"img/vectra-logo.png\">VECTRA SERVER 0.1<br>" . date("Y\-m\-d H\:i\:s") . "<br>Copyright &copy;2014 Warnimont Pol</pre>";
