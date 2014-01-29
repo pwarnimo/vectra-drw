fc8b258040adba9227ee0d5b4ae685aaf5a7e3fe
Pol Warnimont <pwarnimo@gmail.com>
2014-01-29 10:42:05 +0100

    T61 -- CHK12

--- inc/vs.class.php
+++ inc/vs.class.php
@@ -18,7 +18,49 @@ class VectraServer {
 		}
 	}
 	
+	public function getAvailableDrawings() {
+		$xml = new DOMDocument("1.0");
+		
+		$root = $xml->createElement("message");
+		
+		$cmd = $xml->createAttribute("cmd");
+		$cmd->value = "drawings";
+		
+		$drawing = $xml->createAttribute("drawing");
+		$drawing->value = "";
+		
+		$user = $xml->createAttribute("user");
+		
+		$root->appendChild($cmd);
+	
+		try {
+			$qry = "SELECT * FROM tblDrawing";
+			
+			$stmt = $this->dbh->prepare($qry);
+			$stmt->execute();
+			
+			$res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+			
+			foreach ($res as $row) {
+				$drawing = $xml->createElement("drawing");
+				$did = $xml->createAttribute("id");
+				$did->value = $row["idDrawing"];
+				
+				$drawing->appendChild($did);
+				$root->appendChild($drawing);
+			}
+			
+			$xml->appendChild($root);
+			
+			return $xml->saveXML();
+		}
+		catch(PDOException $e) {
+			echo "<pre>PDO has encountered an error: " + $e->getMessage() + "</pre>";
+			die();
+		}
+	}
+	
 	public function debug() {
-		echo "<pre>VECTRA SERVER OK</pre>"
+		echo "<pre>VECTRA SERVER OK</pre>";
 	}
 }
--- index.php
+++ index.php
@@ -3,6 +3,8 @@ include "inc/vs.class.php";
 
 $vcs = new VectraServer();
 
-$vcs->debug();
+//$vcs->debug();
+Header("Content-type: text/xml");
+echo $vcs->getAvailableDrawings();
 
-echo "<pre><img style=\"float:left; margin-right:5px;\" src=\"img/vectra-logo.png\">VECTRA SERVER 0.1<br>" . date("Y\-m\-d H\:i\:s") . "<br>Copyright &copy;2014 Warnimont Pol</pre>";
+//echo "<pre><img style=\"float:left; margin-right:5px;\" src=\"img/vectra-logo.png\">VECTRA SERVER 0.1<br>" . date("Y\-m\-d H\:i\:s") . "<br>Copyright &copy;2014 Warnimont Pol</pre>";
